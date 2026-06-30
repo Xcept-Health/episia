2eb5acb4286624768f831485af132644e608e7e1
ArielShadrac <arielshadrac@gmail.com>
2026-06-30 02:38:33 +0000

    update: init.py

--- src/episia/compatibility/__init__.py
+++ src/episia/compatibility/__init__.py
@@ -8,14 +8,29 @@ Submodules
 coding
     CodingComparator  - quantify the impact of coding differences on
                         epidemiological measures (ICD-10 vs ICD-11, etc.)
+comorbidity
+    ComorbidityScorer     - compute Charlson / Elixhauser scores for a dataset
+    ComorbidityComparator - compare scores between two coding versions
 """
 
-from .coding import CodingComparator
+from .coding import CodingComparator, ComparisonMode
 from .results import CodingRobustnessResult, MeasureDelta
-from .comorbidity import ComorbidityScorer, ComorbidityComparator
+from .comorbidity import (
+    ComorbidityScorer,
+    ComorbidityComparator,
+    ComorbidityScoreResult,
+    ComorbidityComparisonResult,
+)
 
 __all__ = [
+    # coding
     "CodingComparator",
+    "ComparisonMode",
     "CodingRobustnessResult",
     "MeasureDelta",
-]   
+    # comorbidity
+    "ComorbidityScorer",
+    "ComorbidityComparator",
+    "ComorbidityScoreResult",
+    "ComorbidityComparisonResult",
+]
